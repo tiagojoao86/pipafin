80f4cc860d591041e924214562af1b64c627da69
Tiago J Pereira <pereira.tiagojoao@gmail.com>
2024-05-01 08:35:45 -0300

    configure dependecies

---
+++ src/main/java/br/com/grupopipa/pipafin/entity/Titulo.java
@@ -0,0 +1,9 @@
+package br.com.grupopipa.pipafin.entity;
+
+import br.com.grupopipa.pipaonecommons.entity.EntityBase;
+import jakarta.persistence.Entity;
+
+@Entity
+public class Titulo extends EntityBase {
+
+}
