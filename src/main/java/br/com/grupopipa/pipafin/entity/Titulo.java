b5a987ace73163d52a630cacd25d9e9fd47e4ad8
Tiago J Pereira <pereira.tiagojoao@gmail.com>
2024-08-31 23:52:31 -0300

    reset

--- src/main/java/br/com/grupopipa/pipafin/entity/Titulo.java
+++
@@ -1,9 +0,0 @@
-package br.com.grupopipa.pipafin.entity;
-
-import br.com.grupopipa.pipaonecommons.entity.EntityBase;
-import jakarta.persistence.Entity;
-
-@Entity
-public class Titulo extends EntityBase {
-
-}
